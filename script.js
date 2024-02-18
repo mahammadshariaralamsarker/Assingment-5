e7d7ab71d469025db4836e2a8435ab23873c63b3
mahammadshariaralamsarker <154776315+mahammadshariaralamsarker@users.noreply.github.com>
2024-02-18 18:23:46 +0600

    6th

--- script.js
+++ script.js
@@ -2,7 +2,16 @@ let totalSelectedSeat = 0;
 var buttons = document.getElementsByClassName("add-btn");
 for (let i = 0; i < buttons.length; i++) {
     buttons[i].addEventListener("click", function () {
-        this.style.backgroundColor = "green";
+        // this.style.backgroundColor = "red";
+        // this.classlist.add("red");
+        // add color to button by fuction
+        // buttons[i].classList.add("green");
+
+        // Change the background color of the button
+        buttons[i].style.backgroundColor = "#1DD100"; // Change this to whatever color you want
+
+        // Disable the button
+        buttons[i].disabled = true;
         const ticketName = this.innerText;
         const h2 = document.createElement("h2");
         h2.innerText = ticketName;
@@ -16,7 +25,6 @@ for (let i = 0; i < buttons.length; i++) {
         SeletedSeatTypeContainer.appendChild(h3);
 
         const SelectedSeatPrices = document.querySelector('.SelectedSeatPrice').innerText;
-        // console.log(SelectedSeatPrice);
         const SelectedSeatPriceContainer = document.querySelector('.SelectedSeatPriceContainer');
         const h4 = document.createElement("p");
         h4.innerText = SelectedSeatPrices;
@@ -24,29 +32,22 @@ for (let i = 0; i < buttons.length; i++) {
         totalSelectedSeat = totalSelectedSeat + 1;
         const SelectedSeatUnitElement = document.getElementById('SelectedSeatUnit');
         SelectedSeatUnitElement.innerText = totalSelectedSeat;
-        // console.log(totalSelectedSeat);
         const RemainingAvailableSeatElement = document.getElementById('RemainingAvailableSeat');
         const RemainingAvailableSeatText = RemainingAvailableSeatElement.innerText;
-        // console.log(typeof RemainingAvailableSeat);
         const RemainingAvailableSeats = parseInt(RemainingAvailableSeatText);
-        // console.log(RemainingAvailableSeats);
         const remainSeat = RemainingAvailableSeats - 1;
-        // console.log(remainSeat);
         RemainingAvailableSeatElement.innerText = remainSeat;
-        // const seats = 
-        // const RemainingAvailableSeat = 40 - RemainingAvailableSeats;
-        // console.log(RemainingAvailableSeat);
-
-        // RemainingAvailableSeattext.innerText = "30";
-        // console.log(RemainingAvailableSeats);
-        // console.log(totalSelectedSeat);
-        if (totalSelectedSeat > 4) {
-            alert('You can not select more than 4 seat');
-        }
         
+        if (totalSelectedSeat > 4) {
+            
+             return alert('You can not select more than 4 seat');
+            
+        }
         totalFair = totalSelectedSeat * 550;
         const totalFairContainer = document.querySelector('.totalFair');
         totalFairContainer.innerText = totalFair;
         console.log(totalFair);
+        
+
     });
 }
